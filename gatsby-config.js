b4b829f31e93f9d1f1e7bb2f461101a3d608a2c6
Jen <30688276+helplah@users.noreply.github.com>
2019-04-02 21:37:46 +0800

    update layout, bio, social media accounts, profile pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Technical blog`,
+    author: `Jenssen Lee`,
+    description: `A technical blog on the problems I faced and how I solved it`,
+    siteUrl: `https://jenlky.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jenlky`,
+      portfolio: `jenlky`,
+      github: `helplah`,
     },
   },
   plugins: [
--- src/components/layout.js
+++ src/components/layout.js
@@ -14,6 +14,7 @@ class Layout extends React.Component {
         <h1
           style={{
             ...scale(1.5),
+            // Technical Blog marginBottom
             marginBottom: rhythm(1.5),
             marginTop: 0,
           }}
@@ -57,6 +58,7 @@ class Layout extends React.Component {
           marginLeft: `auto`,
           marginRight: `auto`,
           maxWidth: rhythm(24),
+          // padding of whole div
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
@@ -66,7 +68,7 @@ class Layout extends React.Component {
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </footer> 
       </div>
     )
   }
