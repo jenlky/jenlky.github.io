d1dfc0a7a6c8ab45153ee876057ef28ac0f57870
Jen <30688276+helplah@users.noreply.github.com>
2019-04-30 19:25:00 +0800

    add and update blog, portfolio, about page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -18,28 +18,9 @@ class BlogIndex extends React.Component {
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
-        })}
+        <ul>
+          <Link to="/">Home</Link>
+        </ul>
       </Layout>
     )
   }
@@ -70,3 +51,4 @@ export const pageQuery = graphql`
     }
   }
 `
+{/* description below title */}
