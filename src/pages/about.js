78ce66df32d256ae8921229567a19d03174f45ee
Jen <30688276+helplah@users.noreply.github.com>
2019-05-02 15:03:44 +0800

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import NavLinks from "../components/navLinks"
 
-class BlogIndex extends React.Component {
+class About extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
@@ -18,26 +18,33 @@ class BlogIndex extends React.Component {
         />
         <h1>About</h1>
         <p>I am Jenssen Lee Kang Yuan, a Software Engineer in Singapore. 
-          I believe that Software is Eating the World...</p> 
-
-        <h3>*Writing in progress*</h3>
-
-        <p>This site is meant to consolidate writings of my experience 
-          and lessons learnt from doing tech stuff, 
-          and to display any projects I have done.</p>
-        <p>
+          I believe that Software is Eating the World and will continue to do so. 
+          And I choose to be a part of it.
+        </p> 
+        <p>This site is meant to consolidate writings of my experience and lessons learnt from doing tech stuff, 
+          and to showcase my prior projects. You can contact me at&nbsp; 
           <span>
-            Twitter: <a href="https://twitter.com/Jenlky">@jenlky</a>&nbsp; 
+            <a href="mailto:jenssen.lee@gmail.com">jenssen.lee@gmail.com</a>
+          </span>
+          , I&nbsp;
+          <span>
+            <a href="https://jenlky.wordpress.com/">write</a>
+          </span>
+          &nbsp;for leisure too.
+        </p>
+        <p className="social-media">
+          <span>
+            Github: <a href="https://github.com/helplah">@helplah</a>
           </span>
           <span>
-            Github: <a href="https://github.com/helplah">@helplah</a>&nbsp; 
+            Twitter: <a href="https://twitter.com/Jenlky">@jenlky</a> 
           </span>
           <span>
-            Wordpress: <a href="https://jenlky.wordpress.com/">@jenlky</a>&nbsp; 
+            LinkedIn: <a href="https://www.linkedin.com/in/jenlky/">@jenlky</a> 
           </span>
         </p>
         <p>I'm familiar with HTML, CSS, SASS, BootStrap, JavaScript, jQuery, React, React-Router, Redux, 
-          &nbsp;VueJS, Material-UI, Nodejs, MongoDB and git.</p>
+          VueJS, Material-UI, Nodejs, Express, MongoDB and git.</p>
         <p>At the time of writing, I'm on week 3 of&nbsp; 
           <a href="https://www.coursera.org/learn/algorithms-part1/home/welcome">Princeton Algorithms</a> 
           (<a href="https://github.com/helplah/princeton-algorithms">Github repo</a>)
@@ -50,7 +57,7 @@ class BlogIndex extends React.Component {
   }
 }
 
-export default BlogIndex
+export default About
 
 export const pageQuery = graphql`
   query {
