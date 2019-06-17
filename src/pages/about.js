618bd3de39464ac1a6f0210192ddf5fb3229cf35
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-18 06:57:35 +0800

    Update my about page to highlight my current enrolment in JumpStart

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,12 +17,19 @@ class About extends React.Component {
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <h1>About</h1>
-        <p>I am Jenssen Lee Kang Yuan, a Software Engineer in Singapore. 
-          I believe that Software is Eating the World and will continue to do so. 
-          And I choose to be a part of it.
-        </p> 
-        <p>This site is meant to consolidate writings of my experience and lessons learnt from doing tech stuff, 
-          and to showcase my prior projects. You can contact me at&nbsp; 
+        <p>
+          I am Jenssen Lee Kang Yuan, a Software Engineer in Singapore.
+          Currently enrolled in Thoughtworks JumpStart, a 3 months bootcamp.
+          Before that, I had been learning for over a year on my own.
+        </p>
+        <p>
+          I chose to work with software because I believe that Software is
+          Eating the World and I want to be a part of it.
+        </p>
+        <p>
+          This site is meant to consolidate writings of my experience and
+          lessons learnt from doing tech stuff, and to showcase my prior
+          projects. You can contact me at&nbsp;
           <span>
             <a href="mailto:jenssen.lee@gmail.com">jenssen.lee@gmail.com</a>
           </span>
@@ -37,20 +44,28 @@ class About extends React.Component {
             Github: <a href="https://github.com/helplah">@helplah</a>
           </span>
           <span>
-            Twitter: <a href="https://twitter.com/Jenlky">@jenlky</a> 
+            Twitter: <a href="https://twitter.com/Jenlky">@jenlky</a>
           </span>
           <span>
-            LinkedIn: <a href="https://www.linkedin.com/in/jenlky/">@jenlky</a> 
+            LinkedIn: <a href="https://www.linkedin.com/in/jenlky/">@jenlky</a>
           </span>
         </p>
-        <p>I'm familiar with HTML, CSS, SASS, BootStrap, JavaScript, jQuery, React, React-Router, Redux, 
-          VueJS, Material-UI, Nodejs, Express, MongoDB and git.</p>
-        <p>At the time of writing, I'm on week 3 of&nbsp; 
-          <a href="https://www.coursera.org/learn/algorithms-part1/home/welcome">Princeton Algorithms</a> 
-          (<a href="https://github.com/helplah/princeton-algorithms">Github repo</a>)
-          a Coursera MOOC that teaches in Java;
-          while waiting to join ThoughtWorks JumpStart, a three months coding bootcamp, in May. 
+        <p>
+          I'm familiar with HTML, CSS, SASS, BootStrap, JavaScript, jQuery,
+          React, React-Router, Redux, VueJS, Material-UI, Nodejs, Express,
+          MongoDB and git bash.
         </p>
+        {/* <p>
+          At the time of writing, I'm on week 3 of&nbsp;
+          <a href="https://www.coursera.org/learn/algorithms-part1/home/welcome">
+            Princeton Algorithms
+          </a>
+          (
+          <a href="https://github.com/helplah/princeton-algorithms">
+            Github repo
+          </a>
+          ) a Coursera MOOC that teaches in Java.
+        </p> */}
         <NavLinks />
       </Layout>
     )
