aca15940bc54aa0b84c1a07a04a2f5a8f089906f
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-08-03 23:17:28 +0800

    Add logs folder to content

---
+++ src/pages/logs.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import NavLinks from "../components/navLinks"
+import { rhythm } from "../utils/typography"
+
+class Logs extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="All posts" keywords={[`logs`, `javascript`, `react`]} />
+        <h1>Logs</h1>
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginTop: rhythm(3 / 4),
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              <p
+                style={{ marginBottom: `21px` }}
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </div>
+          )
+        })}
+        <NavLinks />
+      </Layout>
+    )
+  }
+}
+
+export default Logs
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: { fields: { slug: { regex: "/^/logs/" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "Do MMMM YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
