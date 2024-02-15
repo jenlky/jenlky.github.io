bb7e2f521efd62be64dfd825a85b01b5dee4b361
Jenssen <jenssen.lee@gmail.com>
2024-02-15 23:15:04 +0800

    Remove about from navigation links

--- src/components/navLinks.js
+++ src/components/navLinks.js
@@ -9,9 +9,6 @@ const NavLinks = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
           <Link to="/blog">Blog</Link>
         </li>
         <li>
