ffef36a6f6d9affd2b8f214c538ac90ae372cff7
Jenssen <jenssen.lee@gmail.com>
2024-02-15 22:50:26 +0800

    Move about me to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
 import { rhythm } from "../utils/typography"
 
 import Layout from "../components/layout"
@@ -12,23 +13,71 @@ class Index extends React.Component {
     const posts = data.allMarkdownRemark.edges
     console.log("data", data)
 
+    let header = data => {
+      return (
+        <Image
+          fixed={data.avatar.childImageSharp.fixed}
+          alt={data.site.siteMetadata.author}
+          style={{
+            marginLeft: rhythm(1 / 2),
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            minWidth: 50,
+          }}
+        />
+      )
+    }
+
     return (
       <Layout location={data.location} title={siteTitle}>
         <SEO
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <h1 className="home-name">Jenssen Lee</h1>
-        <p className="home-job-title">Software Engineer</p>
-        <p>My name is Jenssen Lee or @jenlky on the internet.</p>
+        <div className="home-intro">
+          <h1 className="home-name">Jenssen Lee</h1>
+          <p className="home-job-title">Software Engineer {" "} <sup className="home-hire">(Available for hire)</sup></p>
+        </div>
+        <div className="about-intro">
+          <div>
+            <p>Hi, my name is Jenssen Lee or my friends call me Jens!</p>
+            <p>
+              I'm a Singapore-based Software Engineer who has 4 years of working experience. 
+              Here's a little bit about myself: I'm an avid runner and I've travelled overseas to run a marathon before.
+            </p>
+            <p>
+              My background is from a fintech SME and my expertise lies in JavaScript.
+              I started out learning to code on my own and enrolled in ThoughtWorks Jumpstart, a 3 months coding bootcamp. 
+              After that I got a job and the rest is history.
+            </p>
+            <p>
+              What got me interested was because software is eating the world and I wanted to be a part of it. 
+              I enjoy the process of problem-solving and writing clean code.
+            </p>
+          </div>
+          <header>{header(data)}</header>
+        </div>
         <p>
-          I'm a Software Engineer in Singapore.{" "}
-          <sup className="home-hire">(Available for hire)</sup>
+          You can contact me at&nbsp;
+          <span>
+            <a href="mailto:jenssen.lee@gmail.com">jenssen.lee@gmail.com</a>
+          </span>
+          {/* , I&nbsp;
+          <span>
+            <a href="https://jenlky.wordpress.com/">write</a>
+          </span>
+          &nbsp;for leisure too. */}
         </p>
-        <p>
-          This site contains my essays on technology, software development and
-          whatever is on my mind at the moment. The projects page holds a
-          portfolio of my projects.
+        <p className="social-media">
+          <span>
+            <a href="https://github.com/jenlky">Github</a>
+          </span>
+          {/* <span>
+            <a href="https://twitter.com/Jenlky">Twitter</a>
+          </span> */}
+          <span>
+            <a href="https://www.linkedin.com/in/jenlky/">LinkedIn</a>
+          </span>
         </p>
         <h3 className="recent-essays">Recent Essays</h3>
         {posts.map(({ node }) => {
@@ -62,6 +111,13 @@ export default Index
 
 export const pageQuery = graphql`
   query {
+    avatar: file(absolutePath: { regex: "/taipei-marathon.png/" }) {
+      childImageSharp {
+        fixed(width: 250, height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
     site {
       siteMetadata {
         title
